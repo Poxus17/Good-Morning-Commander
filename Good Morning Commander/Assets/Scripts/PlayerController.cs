2a1c71523035ad24ca1a07a767dd72dd3b702ca2
Poxus <rexer.p.dj@gmail.com>
2021-03-04 15:47:31 +0200

    Revert "UI"
    This reverts commit 5aa8e5bde1bf296247192a8b12fe6e0f147be38e.

--- Good Morning Commander/Assets/Scripts/PlayerController.cs
+++ Good Morning Commander/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,16 @@ public class PlayerController : MonoBehaviour
     NavMeshAgent navMeshAgent;
     Camera cam;
     bool isMoving;
-    
+
     public delegate void MoveStatusChange(bool status);
     public static event MoveStatusChange OnMoveStatusChanged;
 
-    //bool allowCouch = false;
-    public GameObject canvasObject;
-    bool isCanvas = false;
-        
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
         isMoving = false;
-        canvasObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -55,35 +50,12 @@ public class PlayerController : MonoBehaviour
                 {
                     OnMoveStatusChanged(true);
                 }
-
-                if (hit.transform.tag == "Couch")
-                {
-                    //Ink on/off code goes here?
-                    Debug.Log("[Couch] (Ink on/off code)");
-                    MakeActive();
-                }
             }
         }
 
-        void MakeActive()
-        {
-            if (isCanvas == false)
-            { canvasObject.SetActive(true); isCanvas = true; return;} 
-            if (isCanvas == true)
-            { canvasObject.SetActive(false); isCanvas = false; } 
-        }
-
-        //void OnCollisionEnter(Collision collision)
-        //{
-        //    if (collision.gameObject.tag == "Couch_Distance")
-        //    {
-        //        allowCouch = true;
-        //        Debug.Log("Allow Couch");
-        //    }
-        //    else { allowCouch = false; }
-
-        //}
+        
 
 
+        
     }
-    }
+}
