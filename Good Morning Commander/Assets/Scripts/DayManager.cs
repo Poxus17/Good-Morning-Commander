2eb1786288fd612f44e1ae98ec356191f43eda61
mtklng <78037706+mtklng@users.noreply.github.com>
2021-03-29 11:21:55 +0300

    Day Manager
    * Clicking the bed now "passes the day number" randomly (does nothing else right now)

---
+++ Good Morning Commander/Assets/Scripts/DayManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayManager : MonoBehaviour
+{
+    public int day = 0;
+    public int MinDay = 0;
+    public int MaxDay = 0;
+
+    public void DayUp()
+    {
+        day += 1*(Random.Range(MinDay,MaxDay));
+        print("Day " + day);
+    }
+
+}
---
+++ Good Morning Commander/Assets/Scripts/DayPasser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayPasser : MonoBehaviour
+{
+    public DayManager dayManager;
+    private void OnMouseDown()
+    {
+        dayManager.DayUp();
+    }
+}
